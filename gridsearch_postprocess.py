7e192acfda1263c9a3c2c971f9d3ac1d8d0f3dc2
luiscaballerodiaz <luis.caballero7@gmail.com>
2023-03-19 21:07:26 +0100

    Refactoring in grid search output assessment

--- gridsearch_postprocess.py
+++ gridsearch_postprocess.py
@@ -13,6 +13,8 @@ class GridSearchPostProcess:
         self.subplot_row = 2
         self.models = []
         self.dicts = []
+        self.model_step = 'estimator'
+        self.pre_step = 'preprocess'
 
     def param_sweep_matrix(self, params, test_score):
         """Postprocess the cross validation grid search results and plot the parameter sweep"""
@@ -55,7 +57,7 @@ class GridSearchPostProcess:
         - Dicts: list of dicts (one per each model) with the sweep parametrization"""
 
         for i in range(len(params)):
-            string = str(params[i]['estimator'])
+            string = str(params[i][self.model_step])
             model = string[:string.index('(')]
             if model not in self.models:
                 self.models.append(model)
@@ -68,7 +70,7 @@ class GridSearchPostProcess:
             else:
                 self.dicts[index]['test'] = [test_score[i]]
             for key, value in params[i].items():
-                if key == 'estimator':
+                if key == self.model_step:
                     continue
                 elif '__' in key:
                     key = key.replace(key[:key.index('__') + 2], '')
