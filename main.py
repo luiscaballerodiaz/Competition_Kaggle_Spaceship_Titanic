729a003161c2bd4fa1bebf144a19a9b372258da8
luiscaballerodiaz <luis.caballero7@gmail.com>
2023-03-19 03:30:07 +0100

    Implement pipeline grid search

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import pandas as pd
 from data_visualization import DataPlot
+from gridsearch_postprocess import GridSearchPostProcess
 import utils
 
 
 visualization = DataPlot()  # Instantiate an object for DataPlot to manage plots in this exercise
+sweep = GridSearchPostProcess()  # Instantiate an object for GridSearchPostProcess to manage the grid search results
 pd.set_option('display.max_columns', None)  # Enable option to display all dataframe columns
+pd.set_option('display.max_colwidth', None)  # Enable printing the whole column content
 df = pd.read_csv('train.csv')  # Read CSV file
 
 column_target = 'Transported'  # Name for the target feature
-feat_cat = ['HomePlanet', 'CryoSleep', 'Deck', 'Num', 'Side', 'Destination', 'VIP']  # List for the categorical features
-feat_num = ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']  # List for the numerical features
+# List for the categorical features
+feat_cat = ['HomePlanet', 'CryoSleep', 'Deck', 'Num', 'Side', 'Destination', 'VIP']
+# List for the numerical features
+feat_num = ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
 
-df = utils.data_analytics(visualization, df, column_target, feat_cat, feat_num)  # Scrub data and generate plots
-x_train, x_test, y_train, y_test = utils.split_scaling(df, column_target, feat_cat, feat_num)  # Data split and scaling
-utils.modeling(x_train, x_test, y_train, y_test)  # Create model and make predictions
+# Scrub data and generate plots
+df = utils.data_analytics(visualization, df, column_target, feat_cat, feat_num)
+
+# Data split and scaling
+x_train, x_test, y_train, y_test, index_cat, index_num = utils.onehot_split(df, column_target, feat_cat, feat_num)
+
+params = [
+    {'preprocess': ['std'], 'estimator': ['knn'], 'estimator__n_neighbors': [1, 3, 5, 10, 15, 25]},
+    {'preprocess': ['std'], 'estimator': ['logreg'],
+     'estimator__C': [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]},
+    {'preprocess': ['std'], 'estimator': ['linearsvc'],
+     'estimator__C': [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]},
+     {'preprocess': ['', 'std', 'norm'], 'estimator': ['bernoulli']},
+     {'preprocess': ['', 'std', 'norm'], 'estimator': ['gaussian']},
+     {'preprocess': ['', 'std', 'norm'], 'estimator': ['multinomial']},
+    {'preprocess': [''], 'estimator': ['tree'], 'estimator__max_depth': [3, 5, 7, 10, 15, 25, 50]},
+    {'preprocess': [''], 'estimator': ['random forest'], 'estimator__n_estimators': [50, 100, 150, 200],
+     'estimator__max_depth': [5, 10, 15, 25], 'estimator__max_features': [10, 20, 40, 80]},
+    {'preprocess': [''], 'estimator': ['gradient boosting'], 'estimator__n_estimators': [50, 100, 150, 200],
+     'estimator__max_depth': [2, 4, 6, 8, 10], 'estimator__learning_rate': [0.05, 0.075, 0.1, 0.3, 0.5]},
+    {'preprocess': ['std'], 'estimator': ['svm'],
+     'estimator__gamma': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100], 'estimator__C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]},
+    {'preprocess': ['std'], 'estimator': ['mlp'], 'estimator__alpha': [0.001, 0.01, 0.1, 1, 10],
+     'estimator__activation': ['relu'], 'estimator__hidden_layer_sizes': [250, 500, [250, 250], [500, 500]]}]
+
+# Create model, make predictions and plot results
+pd_grid = utils.pipeline_gridsearch(x_train, x_test, y_train, y_test, index_num, params, nfolds=5, scoring='accuracy')
+sweep.param_sweep_matrix(params=pd_grid['params'], test_score=pd_grid['mean_test_score'])
--- utils.py
+++ utils.py
@@ -1,52 +1,125 @@
 import pandas as pd
 import numpy as np
+import time
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
 
 
-def modeling(x_train, x_test, y_train, y_test):
-    model = LogisticRegression(random_state=0)  # Create model
-    param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100]}  # Define model parameter sweep
-    grid_search = GridSearchCV(model, param_grid, cv=5)  # Define grid search with 5 splits cross validation
+def pipeline_gridsearch(x_train, x_test, y_train, y_test, index_num, params, nfolds, scoring):
+    time0 = time.time()
+    param_grid = decode_gridsearch_params(params, index_num)
+    pipe = Pipeline([('preprocess', []), ('estimator', [])])
+    grid_search = GridSearchCV(pipe, param_grid, cv=nfolds, scoring=scoring)  # Define grid search cross validation
     grid_search.fit(x_train, y_train)  # Fit grid search for training set
-    print('LOGISTIC REGRESSION')
-    print("Best parameters: {}".format(grid_search.best_params_))  # Show best parameters
-    print("Best cross-validation score: {:.4f}".format(grid_search.best_score_))  # Show best scores
-    print(pd.DataFrame(grid_search.cv_results_)[['mean_test_score', 'params']])  # Show mean cross validation scores
+    # Save a CSV file with the results for the grid search
+    pd_grid = pd.DataFrame(grid_search.cv_results_)[['mean_test_score', 'params']]
+    print(pd_grid)
+    print('\nGrid search time: {:.1f}\n'.format(time.time() - time0))  # Calculate grid search timing
+    print("Best parameters: {}\n".format(grid_search.best_params_))  # Show best parameters
+    print("Best cross-validation score: {:.4f}\n".format(grid_search.best_score_))  # Show best scores
+    pd_grid.to_csv('Results.csv', mode='w+', index=False)
     model = grid_search.best_estimator_  # Create model with best parametrization
-    print('BEST MODEL TEST SCORE: {:.4f}'.format(model.score(x_test, y_test)))  # Calculate testing score for best model
+    print('\nBEST MODEL TEST SCORE: {:.4f}\n'.format(model.score(x_test, y_test)))  # Calculate best model testing score
+    return pd_grid
 
 
-def split_scaling(df, column_target, feat_cat, feat_num):
+def create_preprocess(pre, columns):
+    if 'norm' in pre.lower():
+        preprocess = ColumnTransformer(transformers=[('scaling', MinMaxScaler(), columns)], remainder='passthrough')
+    elif 'std' in pre.lower():
+        preprocess = ColumnTransformer(transformers=[('scaling', StandardScaler(), columns)], remainder='passthrough')
+    else:
+        preprocess = None
+        print('WARNING: no preprocessor was selected\n')
+    return preprocess
+
+
+def create_model(algorithm):
+    if 'knn' in algorithm.lower():
+        model = KNeighborsClassifier()
+    elif 'logistic' in algorithm.lower() or 'logreg' in algorithm.lower():
+        model = LogisticRegression(random_state=0)
+    elif 'linear svc' in algorithm.lower() or 'linearsvc' in algorithm.lower():
+        model = LinearSVC(random_state=0, dual=False)
+    elif 'gaussian' in algorithm.lower():
+        model = GaussianNB()
+    elif 'bernoulli' in algorithm.lower():
+        model = BernoulliNB()
+    elif 'multinomial' in algorithm.lower():
+        model = MultinomialNB()
+    elif 'tree' in algorithm.lower():
+        model = DecisionTreeClassifier(random_state=0)
+    elif 'forest' in algorithm.lower() or 'random' in algorithm.lower():
+        model = RandomForestClassifier(random_state=0)
+    elif 'gradient' in algorithm.lower() or 'boosting' in algorithm.lower():
+        model = GradientBoostingClassifier(random_state=0)
+    elif 'svm' in algorithm.lower():
+        model = SVC(random_state=0)
+    elif 'mlp' in algorithm.lower():
+        model = MLPClassifier(random_state=0)
+    else:
+        print('\nERROR: Algorithm was NOT provided. Note the type must be a list.\n')
+        model = None
+    return model
+
+
+def decode_gridsearch_params(params, columns):
+    """Assess the input grid search params defined by the user and transform them to the official sklearn estimators"""
+    for i in range(len(params)):
+        model = []
+        for j in range(len(params[i]['estimator'])):
+            model.append(create_model(params[i]['estimator'][j]))
+        params[i]['estimator'] = model
+        preproc = []
+        for j in range(len(params[i]['preprocess'])):
+            preproc.append(create_preprocess(params[i]['preprocess'][j], columns))
+        params[i]['preprocess'] = preproc
+    return params
+
+
+def onehot_split(df, column_target, feat_cat, feat_num):
     # Sanity check: transform categorical features to string
     for cat in feat_cat:
         df[cat] = df[cat].astype(str)
     # Apply one hot encoding to categorical features
     df = pd.get_dummies(df, columns=feat_cat)
     print(f'\nOne hot encoding dataframe size: {df.shape}\n')
-
+    # Transform feat_cat and feat_num from strings names to index numbers
+    for i, column in enumerate(list(df.columns)):
+        if column in feat_cat:
+            feat_cat[feat_cat.index(column)] = i
+        elif column in feat_num:
+            feat_num[feat_num.index(column)] = i
     # Set Target column to individual tensor with 1/0 and remove it from dataframe
     target = df[column_target].replace({True: 1, False: 0})
     df.drop(column_target, axis=1, inplace=True)
     # Create training and testing sets with 80/20% keeping target distribution same as original
     x_train, x_test, y_train, y_test = train_test_split(df, target, test_size=0.2,
                                                         shuffle=True, stratify=target, random_state=0)
+    x_test = np.array(x_test)
+    x_train = np.array(x_train)
     y_test = np.array(y_test)
     y_train = np.array(y_train)
-    # Apply scaling (null mean with unit std) in numerical features (categorical features not modified)
-    scaling = ColumnTransformer(transformers=[('scaling', StandardScaler(), feat_num)], remainder='passthrough')
-    # Fit scaling based on training set and transform it accordingly
-    x_train = scaling.fit_transform(x_train)
-    # Transform testing set accordingly to scaling
-    x_test = scaling.transform(x_test)
     print(f'\nX TRAIN SHAPE: {x_train.shape}\nY TRAIN SHAPE: {y_train.shape}\n')
     print(f'\nX TEST SHAPE: {x_test.shape}\nY TEST SHAPE: {y_test.shape}\n')
 
-    return x_train, x_test, y_train, y_test
+    return x_train, x_test, y_train, y_test, feat_cat, feat_num
 
 
 def data_analytics(visualization, df, column_target, feat_cat, feat_num):
@@ -63,9 +136,9 @@ def data_analytics(visualization, df, column_target, feat_cat, feat_num):
     visualization.pie_plot(df, column_target)  # Plot target class distribution
     # Plot target grouped by individual categorical features (one subplot per categorical feature)
     visualization.cat_features(df, ['HomePlanet', 'CryoSleep', 'Deck', 'Side', 'Destination', 'VIP'], column_target)
-    # Plot target grouped by multiple categorical features (one plot per all categorical features)
+    # # Plot target grouped by multiple categorical features (one plot per all categorical features)
     visualization.cat_features(df, ['Deck', 'Side'], column_target, multi=1)
-    # Histogram for the numerical features
+    # # Histogram for the numerical features
     visualization.num_features(df, feat_num, column_target)
 
     # Check NA values and calculate the affected rows with NA values
